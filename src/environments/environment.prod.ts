4b8aed0336d8ba7ea353dd9a0250edace9849aa7
ramdeveloper <ram1489@gmail.com>
2019-08-10 22:02:08 +0530

    firebase link update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBgZdBYp9cszfUnuQ7g25NPie6M7DkMSuA",
+    authDomain: "ngdotolist.firebaseapp.com",
+    databaseURL: "https://ngdotolist.firebaseio.com",
+    projectId: "ngdotolist",
+    storageBucket: "ngdotolist.appspot.com",
+    messagingSenderId: "228810119253"
+  }
 };
